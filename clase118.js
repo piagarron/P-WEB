becc717c77899aa245ac9b877c7f0e4560610bc1
Pia <pgarron@itba.edu.ar>
2025-08-18 20:57:30 -0300

    .

--- clase118.js
+++ clase118.js
@@ -44,4 +44,46 @@ const ciudades[2] = "Buenos Aires"
 
 const peliculas = 
 
+//6. Declara tres variables booleanas y usa operadores lógicos para 
+// verificar si al menos dos son verdaderas. Imprime true o false en
+//la consola dependiendo del resultado.
+
+
+
+
+//7. Declara tres variables numéricas. Compara si el primer número es 
+// mayor que el segundo y menor que el tercero. Luego, verifica si el
+//  segundo número es distinto al tercero. Imprime los resultados de las 
+// comparaciones.
+
+//8. Declara dos variables que almacenen strings (por ejemplo,
+//  tu nombre y tu apellido). Luego, crea una tercera variable que 
+// concatene las dos primeras y muestra el resultado en la consola.
+
+//9. Declara una variable numérica y utiliza los operadores de 
+// incremento (++) y decremento (--) para modificar su valor. Imprime
+// el valor de la variable después de cada operación.
+
+//10. Crea un objeto que represente un estudiante con propiedades
+//  como nombre, edad y carrera. Accede e imprime cada propiedad del objeto.
+
+//11. Crea un array que contenga dos arrays internos, cada uno con 3 números.
+// Accede al segundo elemento del primer array interno.
+
+//12. Declara dos variables numéricas y utiliza los operadores 
+// >, <, >=, <=, ===, !== para comparar sus valores. Imprime el
+// resultado de cada comparación.
+
+//13. Declara una variable sin asignarle un valor y otra 
+// variable con valor null. Utiliza typeof para imprimir el tipo
+// de cada variable.
+
+//14. Declara una variable que almacene un número como
+//  string (por ejemplo, "123"). Convierte este string a un número usando 
+// parseInt o Number y demuestra que ahora puedes realizar operaciones
+// matemáticas con él.
+
+
+
+
 
